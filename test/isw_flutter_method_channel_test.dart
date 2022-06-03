e61573b8a7348c7c940d879dceaf650cfcec5eb8
Mastersam07 <abadasamuelosp@gmail.com>
2022-06-03 01:29:39 +0100

    :test_tube: chore: add tests

---
+++ test/isw_flutter_method_channel_test.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:isw_flutter/isw_flutter_method_channel.dart';
+import 'package:isw_flutter/models/config.dart';
+import 'package:isw_flutter/models/payment_info.dart';
+import 'package:isw_flutter/models/payment_result.dart';
+
+void main() {
+  MethodChannelIswFlutter platform = MethodChannelIswFlutter();
+  const MethodChannel channel = MethodChannel('isw_flutter');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+  late IswSdkConfig config;
+  late IswPaymentInfo info;
+
+  setUp(
+    () {
+      config = IswSdkConfig(
+        merchantId: 'merchantId',
+        merchantSecret: 'merchantSecret',
+        currencyCode: 'currencyCode',
+        merchantCode: 'merchantCode',
+      );
+      info = IswPaymentInfo(
+          amount: 500,
+          customerEmail: 'customerEmail',
+          customerId: 'customerId',
+          customerMobile: 'customerMobile',
+          customerName: 'customerName',
+          reference: 'reference');
+      channel.setMockMethodCallHandler(
+        (MethodCall methodCall) async {
+          switch (methodCall.method) {
+            case 'getPlatformVersion':
+              return '42';
+            case 'initialize':
+              return true;
+            case 'pay':
+              return null;
+          }
+        },
+      );
+    },
+  );
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await platform.getPlatformVersion(), '42');
+  });
+
+  test('initialize', () async {
+    bool? init = await platform.initialize(config);
+    expect(init, true);
+  });
+
+  test('pay', () async {
+    Optional<IswPaymentResult> pay = await platform.pay(info);
+    expect(pay.runtimeType, Optional<IswPaymentResult>);
+  });
+}
---
+++ test/isw_flutter_test.dart
@@ -0,0 +1,84 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:isw_flutter/enums.dart';
+import 'package:isw_flutter/isw_flutter.dart';
+import 'package:isw_flutter/isw_flutter_platform_interface.dart';
+import 'package:isw_flutter/isw_flutter_method_channel.dart';
+import 'package:isw_flutter/models/config.dart';
+import 'package:isw_flutter/models/payment_result.dart';
+import 'package:isw_flutter/models/payment_info.dart';
+import 'package:isw_flutter/util.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+class MockIswFlutterPlatform
+    with MockPlatformInterfaceMixin
+    implements IswFlutterPlatform {
+  @override
+  Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<bool?> initialize(IswSdkConfig config,
+          [Environment env = Environment.test]) =>
+      Future.value(true);
+
+  @override
+  Future<Optional<IswPaymentResult>> pay(IswPaymentInfo info) =>
+      Future.value(Optional(null, false));
+}
+
+void main() {
+  final IswFlutterPlatform initialPlatform = IswFlutterPlatform.instance;
+
+  late IswSdkConfig config;
+  late IswPaymentInfo info;
+
+  setUp(
+    () {
+      config = IswSdkConfig(
+        merchantId: 'merchantId',
+        merchantSecret: 'merchantSecret',
+        currencyCode: 'currencyCode',
+        merchantCode: 'merchantCode',
+      );
+      info = IswPaymentInfo(
+          amount: 500,
+          customerEmail: 'customerEmail',
+          customerId: 'customerId',
+          customerMobile: 'customerMobile',
+          customerName: 'customerName',
+          reference: 'reference');
+    },
+  );
+
+  test('$MethodChannelIswFlutter is the default instance', () {
+    expect(initialPlatform, isInstanceOf<MethodChannelIswFlutter>());
+  });
+
+  test('getPlatformVersion', () async {
+    IswFlutter iswFlutterPlugin = IswFlutter();
+    MockIswFlutterPlatform fakePlatform = MockIswFlutterPlatform();
+    IswFlutterPlatform.instance = fakePlatform;
+
+    expect(await iswFlutterPlugin.getPlatformVersion(), '42');
+  });
+
+  test('initialize', () async {
+    IswFlutter iswFlutterPlugin = IswFlutter();
+    MockIswFlutterPlatform fakePlatform = MockIswFlutterPlatform();
+    IswFlutterPlatform.instance = fakePlatform;
+
+    expect(await iswFlutterPlugin.initialize(config), true);
+  });
+
+  test('pay', () async {
+    IswFlutter iswFlutterPlugin = IswFlutter();
+    MockIswFlutterPlatform fakePlatform = MockIswFlutterPlatform();
+    IswFlutterPlatform.instance = fakePlatform;
+    Optional<IswPaymentResult> pay = await iswFlutterPlugin.pay(info);
+
+    expect(pay.hasValue, false);
+  });
+
+  test('nameOf', () async {
+    expect(nameOf(Environment.test), 'test');
+  });
+}
